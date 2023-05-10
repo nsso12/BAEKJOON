1b3ee50249ddb6b32f400ffa98c7396d3c79efae
nsso12 <sora628889@gmail.com>
2023-05-10 15:37:06 +0900

    update

--- src/ch03/Loop08.java
+++ src/ch03/Loop08.java
@@ -2,24 +2,24 @@ package ch03;
 
 import java.util.Scanner;
 
-public class Loop07 {
+public class Loop08 {
     public static void main(String[] args) {
         //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-        //각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다.
-        // 테스트 케이스 번호는 1부터 시작한다.
+        //각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다.
+        // x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
 
         Scanner sc = new Scanner(System.in);
-
         int T = sc.nextInt();
 
-        for (int i = 1; i <= T; i++) {
+        for(int i=1; i<T; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
 
-            System.out.println("Case #" + i + ": " + A + " + " + B + " = "+ (A + B));
+            if(0<A && B<10) {
+                System.out.println("Case #"+ i + ": " + (A+B));
+            }
         }
 
-        sc.close();
 
     }
 }
---
+++ src/ch03/Loop10.java
@@ -0,0 +1,26 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Loop10 {
+    public static void main(String[] args) {
+        //첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+        //하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+        //첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+        // 추후 도전 예정
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.println(" ");
+            }
+            for(int j =0; j<i; j++){
+
+            };
+
+
+        }
+    }
+}
